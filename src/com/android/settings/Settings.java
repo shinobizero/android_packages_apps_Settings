c2a0e7fcd08212874f5c1c88762cf8f48ff26677
tsubus <tsubus@ilwt.org>
2014-01-17 10:41:41 +0100

    Add SuperSu to settings dashboard *used root icon from purenexus marshmallow
    Change-Id: I1c088aab1756be7e11a159f7c665fd0bfadd3022

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -163,4 +163,7 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+
+    // ZeroROM Additions.
+    public static class SuperSUActivity extends SettingsActivity { /* empty */ }
 }
