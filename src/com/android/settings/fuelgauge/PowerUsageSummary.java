aa84aa95e1f661b8f72c5a7b44c647bd9b4ea8e9
BeansTown106 <nbenis106@gmail.com>
2015-03-18 11:10:54 -0400

    Skip Misc/Overcounted battery stats regardless of build type.

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -336,7 +336,9 @@ public class PowerUsageSummary extends PowerUsageBase {
                     if (percentOfTotal < 10) {
                         continue;
                     }
-                    if ("user".equals(Build.TYPE) || "userdebug".equals(Build.TYPE)) {
+
+                    if ("user".equals(Build.TYPE) || "userdebug".equals(Build.TYPE) || "eng".equals(Build.TYPE)) {
+
                         continue;
                     }
                 }
@@ -349,7 +351,8 @@ public class PowerUsageSummary extends PowerUsageBase {
                     if (percentOfTotal < 5) {
                         continue;
                     }
-                    if ("user".equals(Build.TYPE) || "userdebug".equals(Build.TYPE)) {
+
+                    if ("user".equals(Build.TYPE) || "userdebug".equals(Build.TYPE) || "eng".equals(Build.TYPE)) {
                         continue;
                     }
                 }
